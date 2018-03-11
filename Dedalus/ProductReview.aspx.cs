d1201559b9b1c52fc28cb67a3ac788b6aa384ffe
koleopteros <11413813+koleopteros@users.noreply.github.com>
2018-03-11 15:49:57 -0400

    Product Feedback forms
    TBD: have a "review" link on product page that redirects to these forms

---
+++ Dedalus/ProductReview.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Default3 : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void btnSubmitReview_Click(object sender, EventArgs e)
+    {
+        if (IsValid)
+        {
+            Response.Redirect("FeedbackSuccess.aspx");
+        }
+    }
+
+    protected void btnReturn_Click(object sender, EventArgs e)
+    {
+        Response.Redirect(Request.UrlReferrer.ToString());
+    }
+}
